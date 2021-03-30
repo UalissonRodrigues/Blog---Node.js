79f9d32366aeb5c56b1e16440a5cf5f048c6ef8a
UalissonRodrigues <ualissonfreitas@hotmail.com>
2021-03-29 21:31:47 -0300

    Crud - Update e View de alteração

--- categoria/categoriaController.js
+++ categoria/categoriaController.js
@@ -9,6 +9,7 @@ router.get("/admin/categoria/new", (req, res) => {
     res.render("admin/Viewscategoria/categoriaViews");
 });
 
+// Rota Para salvar uma categoria 
 router.post("/categoria/salvar", (req, res) => {
     var forms = req.body.title; // Variavel formulario vai receber o que esta vindo no formulario categoria
     if (forms != undefined) {
@@ -16,21 +17,23 @@ router.post("/categoria/salvar", (req, res) => {
             title: forms,
             slug: slugify(forms)
         }).then(() => {
-            res.redirect("/");
+            res.redirect("/admin/categoria");
         })
     } else {
         res.redirect("/admin/categoria/new");
     }
 });
 
+// Rota para Printar todas as Categorias na tela.
 router.get("/admin/categoria", (req, res) => {
     categoriaModel.findAll().then(categoria => {
         res.render("admin/Viewscategoria/indexCategoria", { categoria: categoria });
     });
 });
 
+// Rota Para Deletar Uma Categoria ja postada
 router.post("/categoria/delete", (req, res) => {
-    var id = req.body.id;
+    var id = req.body.id; // Pegando o Id da Categoria Selecionada
     if (id != undefined) {
         if (!isNaN(id)) {
             categoriaModel.destroy({
@@ -50,5 +53,34 @@ router.post("/categoria/delete", (req, res) => {
     }
 });
 
+router.get("/admin/categoria/editar/:id", (req, res) => {
+    var id = req.params.id;
+    if (isNaN(id)) {
+        res.redirect("/admin/categoria");
+    }
+    categoriaModel.findByPk(id).then(categoria => {  // Metodo para pesquisar o Id no banco de dados
+        if (categoria != undefined) {
 
+            res.render("admin/Viewscategoria/categoriaEditar", { categoria: categoria });
+
+        } else {
+            res.redirect("/admin/categoria");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categoria");
+    })
+});
+
+router.post("/categoria/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+
+    categoriaModel.update({ title: title , slug: slugify(title)}, {  //realizando Update  no Campo Titulo
+        where: {
+            id: id
+        }
+    }).then(() =>{
+        res.redirect("/admin/categoria");;
+    })
+});
 module.exports = router;
