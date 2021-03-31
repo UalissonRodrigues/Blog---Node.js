d056e9bd7a7e52591d7f9fa604e1abd967352a22
UalissonRodrigues <ualissonfreitas@hotmail.com>
2021-03-30 22:36:22 -0300

    Desenvolvimento da Tela de cadastro de Artigo utilizando o TinyMCE para dar mais vida ao textArea do blog

--- artigo/artigoController.js
+++ artigo/artigoController.js
@@ -2,9 +2,29 @@
 
 const express = require("express");
 const router = express.Router();
+const categoriaModel = require("../categoria/categoriaModel");
+const artigoModel = require("./artigosModel");
+const slugify = require("slugify");
 
-router.get("/admin/artigo/new", (req,res) =>{
-    res.render("admin/Viewsartigo/artigoViews");
+router.get("/admin/artigo/new", (req, res) => {
+    categoriaModel.findAll().then(categoria => {
+        res.render("admin/Viewsartigo/artigoViews", { categoria: categoria });
+    })
+});
+
+router.post("/admin/artigo/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var categoria = req.body.categoria;
+
+    artigoModel.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoriumId: categoria
+    }).then(() => {
+        res.redirect("/")
+    })
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -1,4 +1,6 @@
 const Sequelize  = require("sequelize");
-const Connection = new Sequelize('blog','root','root'
-,{host:'localhost',dialect:'mysql'});
+const Connection = new Sequelize('blog','root','root',{
+    host:'localhost',
+    dialect:'mysql', 
+    timezone:"-03:00"});
 module.exports = Connection;
